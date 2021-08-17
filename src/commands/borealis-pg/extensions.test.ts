ae9ce00fe5be02d3dbeff7e6b6684260ae056560
Joel Andrews <joel.andrews@borealis-data.com>
2021-08-17 15:58:56 -0700

    PG extension list command
    Introduces the `borealis-pg:extensions` command to obtain a list of installed Postgres extensions for a specified add-on.

---
+++ src/commands/borealis-pg/extensions.test.ts
@@ -0,0 +1,107 @@
+import {expect, test} from '@oclif/test'
+import color from '@heroku-cli/color'
+
+const baseHerokuApiUrl = 'https://api.heroku.com'
+const baseBorealisPgApiUrl = 'https://pg-heroku-addon-api.borealis-data.com'
+const fakeBorealisPgAddonName = 'borealis-pg-my-fake-addon'
+const fakeHerokuAuthToken = 'my-fake-heroku-auth-token'
+const fakeHerokuAuthId = 'my-fake-heroku-auth'
+const fakeExt1 = 'my-first-fake-pg-extension'
+const fakeExt2 = 'my-second-fake-pg-extension'
+
+const baseTestContext = test.stdout()
+  .stderr()
+  .nock(baseHerokuApiUrl, api =>
+    api.post('/oauth/authorizations', {
+      description: 'Borealis PG CLI plugin temporary auth token',
+      expires_in: 120,
+      scope: ['read'],
+    })
+      .reply(200, {id: fakeHerokuAuthId, access_token: {token: fakeHerokuAuthToken}})
+      .delete(`/oauth/authorizations/${fakeHerokuAuthId}`)
+      .reply(200))
+
+describe('extension list command', () => {
+  baseTestContext
+    .nock(
+      baseBorealisPgApiUrl,
+      {reqheaders: {authorization: `Bearer ${fakeHerokuAuthToken}`}},
+      api => api.get(`/heroku/resources/${fakeBorealisPgAddonName}/pg-extensions`)
+        .reply(200, {extensions: [{name: fakeExt1}, {name: fakeExt2}]}))
+    .command(['borealis-pg:extensions', '--addon', fakeBorealisPgAddonName])
+    .it('outputs the list of installed extensions', ctx => {
+      expect(ctx.stderr).to.equal(
+        `Fetching Postgres extension list for add-on ${fakeBorealisPgAddonName}...\n` +
+        `Fetching Postgres extension list for add-on ${fakeBorealisPgAddonName}... done\n`)
+      expect(ctx.stdout).to.equal(`${fakeExt1}\n${fakeExt2}\n`)
+    })
+
+  baseTestContext
+    .nock(
+      baseBorealisPgApiUrl,
+      api => api.get(`/heroku/resources/${fakeBorealisPgAddonName}/pg-extensions`)
+        .reply(200, {extensions: []}))
+    .command(['borealis-pg:extensions', '-o', fakeBorealisPgAddonName])
+    .it('outputs a warning if there are no extensions', ctx => {
+      expect(ctx.stderr).to.equal(
+        `Fetching Postgres extension list for add-on ${fakeBorealisPgAddonName}...\n` +
+        `Fetching Postgres extension list for add-on ${fakeBorealisPgAddonName}... done\n` +
+        ' ›   Warning: No extensions found\n')
+      expect(ctx.stdout).to.equal('')
+    })
+
+  baseTestContext
+    .nock(
+      baseBorealisPgApiUrl,
+      api => api.get(`/heroku/resources/${fakeBorealisPgAddonName}/pg-extensions`)
+        .reply(404, {reason: 'Does not exist'}))
+    .command(['borealis-pg:extensions', '-o', fakeBorealisPgAddonName])
+    .catch(`Add-on ${color.addon(fakeBorealisPgAddonName)} was not found or is not a Borealis Isolated Postgres add-on`)
+    .it('exits with an error if the add-on was not found', ctx => {
+      expect(ctx.stdout).to.equal('')
+    })
+
+  baseTestContext
+    .nock(
+      baseBorealisPgApiUrl,
+      api => api.get(`/heroku/resources/${fakeBorealisPgAddonName}/pg-extensions`)
+        .reply(422, {reason: 'Not ready yet'}))
+    .command(['borealis-pg:extensions', '-o', fakeBorealisPgAddonName])
+    .catch(`Add-on ${color.addon(fakeBorealisPgAddonName)} is not finished provisioning`)
+    .it('exits with an error if the add-on is not done provisioning', ctx => {
+      expect(ctx.stdout).to.equal('')
+    })
+
+  baseTestContext
+    .nock(
+      baseBorealisPgApiUrl,
+      api => api.get(`/heroku/resources/${fakeBorealisPgAddonName}/pg-extensions`)
+        .reply(500, {reason: 'Something went wrong'}))
+    .command(['borealis-pg:extensions', '-o', fakeBorealisPgAddonName])
+    .catch('Add-on service is temporarily unavailable. Try again later.')
+    .it('exits with an error if the Borealis PG API indicates a server error', ctx => {
+      expect(ctx.stdout).to.equal('')
+    })
+
+  test.stdout()
+    .stderr()
+    .nock(
+      baseHerokuApiUrl,
+      api => api.post('/oauth/authorizations')
+        .reply(200, {id: fakeHerokuAuthId})  // The access_token field is missing
+        .delete(`/oauth/authorizations/${fakeHerokuAuthId}`)
+        .reply(200))
+    .command(['borealis-pg:extensions', '-o', fakeBorealisPgAddonName])
+    .catch('Log in to the Heroku CLI first!')
+    .it('exits with an error if there is no Heroku access token', ctx => {
+      expect(ctx.stdout).to.equal('')
+    })
+
+  test.stdout()
+    .stderr()
+    .command(['borealis-pg:extensions'])
+    .catch(/^Missing required flag:/)
+    .it('exits with an error if there is no add-on name flag', ctx => {
+      expect(ctx.stdout).to.equal('')
+    })
+})
---
+++ src/commands/borealis-pg/extensions.ts
@@ -0,0 +1,79 @@
+import * as Heroku from '@heroku-cli/schema'
+import {Command, flags} from '@heroku-cli/command'
+import {HTTP, HTTPError} from 'http-call'
+import color from '@heroku-cli/color'
+import cli from 'cli-ux'
+
+const pgExtensionColour = color.green
+
+export default class ListPgExtensionsCommand extends Command {
+  static description = 'List installed Postgres extensions for a Borealis Isolated Postgres add-on'
+
+  static flags = {
+    addon: flags.string({
+      char: 'o',
+      description: 'The name or ID of a Borealis Isolated Postgres add-on',
+      required: true,
+    }),
+  }
+
+  async run() {
+    const {flags} = this.parse(ListPgExtensionsCommand)
+    const addonName = flags.addon
+    const authResponse = await this.heroku.post<Heroku.OAuthAuthorization>(
+      '/oauth/authorizations',
+      {
+        body: {
+          description: 'Borealis PG CLI plugin temporary auth token',
+          expires_in: 120,
+          scope: ['read'],
+        },
+      })
+
+    try {
+      const accessTokenInfo = authResponse.body.access_token
+      if (!accessTokenInfo) {
+        this.error('Log in to the Heroku CLI first!')
+      } else {
+        cli.action.start(
+          `Fetching Postgres extension list for add-on ${color.addon(addonName)}`)
+        const response = await HTTP.get(
+          `https://pg-heroku-addon-api.borealis-data.com/heroku/resources/${addonName}/pg-extensions`,
+          {
+            headers: {Authorization: `Bearer ${accessTokenInfo.token}`},
+          })
+        cli.action.stop()
+
+        const responseBody = response.body as {extensions: Array<{name: string}>}
+        if (responseBody.extensions.length > 0) {
+          responseBody.extensions.forEach(extension => {
+            this.log(pgExtensionColour(extension.name))
+          })
+        } else {
+          this.warn('No extensions found')
+        }
+      }
+    } finally {
+      await this.heroku.delete<Heroku.OAuthAuthorization>(
+        `/oauth/authorizations/${authResponse.body.id}`)
+    }
+  }
+
+  async catch(err: any) {
+    const {flags} = this.parse(ListPgExtensionsCommand)
+    const addonName = flags.addon
+
+    if (err instanceof HTTPError) {
+      if (err.statusCode === 404) {
+        this.error(
+          `Add-on ${color.addon(addonName)} was not found or is not a Borealis Isolated Postgres add-on`)
+      } else if (err.statusCode === 422) {
+        this.error(`Add-on ${color.addon(addonName)} is not finished provisioning`)
+      } else {
+        this.error('Add-on service is temporarily unavailable. Try again later.')
+      }
+    } else {
+      throw err
+    }
+  }
+}
