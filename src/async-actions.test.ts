adebbee458122a275a3290a180d7e92423f960c1
Joel Andrews <joel.andrews@borealis-data.com>
2021-09-24 16:06:46 -0700

    Additional action spinner tests
    Adds a couple new tests for the action spinner helper function.

--- src/async-actions.test.ts
+++ src/async-actions.test.ts
@@ -5,27 +5,40 @@ import {test} from './test-utils'
 describe('applyActionSpinner', () => {
   test.stdout()
     .stderr()
-    .it('resolves the given promise', async ctx => {
+    .it('resolves the given promise', async () => {
       const expectedResult = 'my-cool-result'
       const promise = Promise.resolve(expectedResult)
 
-      const fakeMessage = 'my-cool-message'
-
-      const result = await applyActionSpinner(fakeMessage, promise)
+      const result = await applyActionSpinner('', promise)
 
       expect(result).to.equal(expectedResult)
+    })
+
+  test.stdout()
+    .stderr()
+    .it('rejects a promise that throws an error', async () => {
+      const expectedError = new Error('my-bad-error')
+      const promise = Promise.reject(expectedError)
+
+      expect(applyActionSpinner('', promise)).to.be.rejectedWith(expectedError)
+    })
+
+  test.stdout()
+    .stderr()
+    .it('outputs the specified message for a successful execution', async ctx => {
+      const fakeMessage = 'my-excellent-message'
+
+      await applyActionSpinner(fakeMessage, Promise.resolve('my-excellent-result'))
+
       expect(ctx.stderr).to.contain(fakeMessage)
     })
 
   test.stdout()
     .stderr()
-    .it('rejects a promise that throws an error', async ctx => {
-      const expectedError = new Error('my-bad-error')
-      const promise = Promise.reject(expectedError)
+    .it('outputs the specified message for a failed execution', async ctx => {
+      const fakeMessage = 'my-terrible-message'
 
-      const fakeMessage = 'my-bad-message'
-
-      expect(applyActionSpinner(fakeMessage, promise)).to.be.rejectedWith(expectedError)
+      expect(applyActionSpinner(fakeMessage, Promise.reject(new Error()))).to.be.rejected
 
       expect(ctx.stderr).to.contain(fakeMessage)
     })
